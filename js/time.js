258b8578a39bed857267bdde6e3015eccb6743ab
Natasha <natasha.pearl08@gmail.com>
2024-08-14 19:19:36 +0100

    Added more time periods to time.js

--- js/time.js
+++ js/time.js
@@ -9,26 +9,28 @@ function styleByTime() {
     var temp;
 
     // Define time ranges for different periods of the day
-    if (h >= 6 && h < 12)
-        temp = "morning"; // 6 AM to 12 PM
+    if (h >= 6 && h < 8)
+        temp = "sunrise"; // 6 AM to 8 AM (Sunrise)
+    else if (h >= 8 && h < 12)
+        temp = "morning"; // 8 AM to 12 PM (Morning)
     else if (h >= 12 && h < 18)
-        temp = "afternoon"; // 12 PM to 6 PM
+        temp = "afternoon"; // 12 PM to 6 PM (Afternoon)
     else if (h >= 18 && h < 20)
-        temp = "sunset"; // 6 PM to 8 PM
+        temp = "sunset"; // 6 PM to 8 PM (Sunset)
     else
-        temp = "night"; // 8 PM to 6 AM
+        temp = "night"; // 8 PM to 6 AM (Night)
 
     if (temp != current) {
         // Update background image based on the time of day
-        if (temp == "morning") {
+        if (temp == "sunrise" || temp == "sunset") {
+            d3.select("body").style("background-image", "url(\"img/bg_sunset.png\")");
+            current = temp;
+        } else if (temp == "morning") {
             d3.select("body").style("background-image", "url(\"img/bg_morning.png\")");
             current = "morning";
         } else if (temp == "afternoon") {
             d3.select("body").style("background-image", "url(\"img/bg_afternoon.png\")");
             current = "afternoon";
-        } else if (temp == "sunset") {
-            d3.select("body").style("background-image", "url(\"img/bg_sunset.png\")");
-            current = "sunset";
         } else if (temp == "night") {
             d3.select("body").style("background-image", "url(\"img/bg_night.png\")");
             current = "night";
